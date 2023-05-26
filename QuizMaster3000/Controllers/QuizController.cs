e88b5f0324e9bb6a9a48b28e4746e20c172ae1a3
ADAMOOD <99761968+ADAMOOD@users.noreply.github.com>
2023-05-26 10:18:35 +0200

    commit

--- QuizMaster3000/Controllers/QuizController.cs
+++ QuizMaster3000/Controllers/QuizController.cs
@@ -4,29 +4,40 @@ using QuizMaster3000.Providers;
 
 namespace QuizMaster3000.Controllers
 {
-	[ApiController]
-	[Route("/api/[controller]")]
-	public class QuizController : ControllerBase
-	{
-		private readonly QuizProvider provider;
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class QuizController : ControllerBase
+    {
+        private readonly QuizProvider provider;
 
-		public QuizController(QuizProvider quizProvider)
-		{
-			provider = quizProvider;
-		}
+        public QuizController(QuizProvider quizProvider)
+        {
+            provider = quizProvider;
+        }
 
-		[HttpGet]
-		[Route("")]
-		public async Task<IActionResult> GetQuizzes()
-		{
-			return Ok(await provider.GetQuizzes());
-		}
-		[HttpPost]
-		[Route("")]
-		public async Task<IActionResult> PostQuiz() //momentalne jen testovaci data, pozdeji asi bude nejake ui
-		{
-			return Ok(await provider.PostQuiz(10,2,RoomState.InLobby));
-		}
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> GetQuizzes()
+        {
+            return Ok(await provider.GetQuizzes());
+        }
 
-	}
+        [HttpGet]
+        [Route("random/{amount}")]
+        public async Task<IActionResult> GetRandomQuizzes([FromRoute] int amount)
+        {
+            Random random = new Random();
+            Thread.Sleep(random.Next(500, 5000));
+            return Ok(await provider.GenerateQuizzes(amount));
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<IActionResult> PostQuiz() //momentalne jen testovaci data, pozdeji asi bude nejake ui
+        {
+            return Ok(await provider.PostQuiz(10, 2, RoomState.InLobby));
+        }
+
+
+    }
 }
--- QuizMaster3000/Providers/QuizProvider.cs
+++ QuizMaster3000/Providers/QuizProvider.cs
@@ -4,21 +4,40 @@ namespace QuizMaster3000.Providers;
 
 public class QuizProvider
 {
-	private static List<Quiz> QuizList = new List<Quiz>();
+    private static List<Quiz> QuizList = new List<Quiz>();
 
-	public QuizProvider()
-	{
+    public QuizProvider()
+    {
 
-	}
-	public async Task<List<Quiz>> GetQuizzes()
-	{
-		return QuizList;
-	}
+    }
+    public async Task<List<Quiz>> GetQuizzes()
+    {
+        return QuizList;
+    }
 
-	public async Task<Quiz> PostQuiz(int maxPlayerCount, int currentPlayerCount, RoomState roomState)
-	{
-		Quiz quiz = new Quiz(maxPlayerCount, currentPlayerCount, roomState);
-		QuizList.Add(quiz);
-		return quiz;
-	}
+    public async Task<Quiz> PostQuiz(int maxPlayerCount, int currentPlayerCount, RoomState roomState)
+    {
+        Quiz quiz = new Quiz(maxPlayerCount, currentPlayerCount, roomState);
+        QuizList.Add(quiz);
+        return quiz;
+    }
+
+    public async Task<List<Quiz>> GenerateQuizzes(int amount)
+    {
+        QuizList.Clear();
+        for (int i = 0; i < amount; i++)
+        {
+            Random random = new Random();
+            var roomStates = (RoomState[])Enum.GetValues(typeof(RoomState));
+
+            var maxPlayerCount = random.Next(1, 20);
+            var currentPlayerCount = maxPlayerCount - random.Next(0, maxPlayerCount);
+            var roomState = roomStates[random.Next(0, roomStates.Length)];
+
+            var quiz = new Quiz(maxPlayerCount, currentPlayerCount, roomState);
+            QuizList.Add(quiz);
+        }
+
+        return QuizList;
+    }
 }
